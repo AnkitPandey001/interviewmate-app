c89967740331ee09301cfa95740f5c3c28d3180c
Ankit Kumar Pandey <143742399+AnkitPandey001@users.noreply.github.com>
2025-04-21 00:33:22 +0530

    Update create-edit-page.tsx

--- src/routes/create-edit-page.tsx
+++ src/routes/create-edit-page.tsx
@@ -1,32 +1,32 @@
 import { FormMockInterviews } from "@/components/form-mock";
-import { db } from "@/config/firebase.config";
-import { Interviews } from "@/types";
-import { doc, getDoc } from "firebase/firestore";
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom"
+// import { db } from "@/config/firebase.config";
+// import { Interviews } from "@/types";
+// import { doc, getDoc } from "firebase/firestore";
+// import { useEffect, useState } from "react";
+// import { useParams } from "react-router-dom"
 
 
 export const CreateEditPages = () =>{
 
-    const {interviewsId} = useParams<{interviewsId:string}>();
-    const [interview,setInterview] = useState<Interviews | null>(null)
+    // const {interviewsId} = useParams<{interviewsId:string}>();
+    // const [interview,setInterview] = useState<Interviews | null>(null)
    
-    useEffect(()=>{
-       const fetchData = async () =>{
-           if(interviewsId){
-            try {
-            const interviewDoc = await getDoc(doc(db,"interviews",interviewsId))
-            if(interviewDoc.exists()){
-                setInterview({...interviewDoc.data} as Interviews)
-            }
-            } catch (error) {
-             console.log(error)
-            }
-           }
-       }
+    // useEffect(()=>{
+    //    const fetchData = async () =>{
+    //        if(interviewsId){
+    //         try {
+    //         const interviewDoc = await getDoc(doc(db,"interviews",interviewsId))
+    //         if(interviewDoc.exists()){
+    //             setInterview({...interviewDoc.data} as Interviews)
+    //         }
+    //         } catch (error) {
+    //          console.log(error)
+    //         }
+    //        }
+    //    }
 
-       fetchData();
-    },[interviewsId])
+    //    fetchData();
+    // },[interviewsId])
    
     return(
         <div className=" my-4 flex-col w-full">
